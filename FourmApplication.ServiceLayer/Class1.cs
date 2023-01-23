4760885fbfdd6a408d3ac10729219fa6bcfe658e
Srikanth B <Srikanth B@LAPTOP-TJ6JAB1H>
2023-01-23 11:06:56 +0530

    Add project files.

---
+++ FourmApplication.ServiceLayer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourmApplication.ServiceLayer
+{
+    public class Class1
+    {
+    }
+}
